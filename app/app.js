d6606823d25e7fc7f7324d3fac3fbb2f53c3a39f
Caine Jette <jette@alum.mit.edu>
2016-03-28 22:41:30 -0700

    Basic styling. Add blinky colon.

--- app/app.js
+++ app/app.js
@@ -1,27 +1,32 @@
 var app = angular.module('yamm', []);
 
 app.controller('timeCtrl', ['$scope', '$interval', function($scope, $interval) {
-  $interval(() => $scope.currentTime = new Date(), 1000);
+  $scope.hideColon = true;
+  
+  $interval(() => {
+    $scope.currentTime = new Date();
+    $scope.hideColon = !$scope.hideColon;
+  }, 1000);
 }]);
 
 app.controller('weatherCtrl', ['$scope', '$interval', 'weatherService', function($scope, $interval, weatherService) {
   fetchWeather = () => {
-    
     weatherService.getWeather().then(data => {
-      $scope.currentTemperature = data.main.temp;
+      $scope.currentTemperature = Math.round(data.main.temp);
     })
     
     weatherService.getForecast().then(data => {
       $scope.forecasts = data.list.map(x => {
         return {
           'time': new Date(x.dt * 1000),
-          'temp': x.main.temp
+          'temp': Math.round(x.main.temp)
         }
       });
     }); 
-  }
+  }  
   
-  $interval(fetchWeather(), 60000);
+  fetchWeather();
+  $interval(fetchWeather, 600000);
 }]);
 
 app.factory('weatherService', ['$http', '$q', function ($http, $q){
