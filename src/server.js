f5a3b356f6c518d55514a99cf259575293fee403
Caine Jette <jette@alum.mit.edu>
2016-04-01 09:05:37 -0700

    Delete completed tasks from todoist to keep list manageable.

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var proxy = require('http-proxy');
 var app = express();
 var curl = require('curlrequest');
+var uuid = require('node-uuid');
 
 var secrets = require('../secrets.json');
 var config = require('../config.json');
@@ -71,31 +72,42 @@ router.get('/api/todo', (req, res) => {
             resource_types: '["items"]'
         }
     }
-   curl.request(options, (err, data) => {
-       if (err) {
-           res.send(err);
-       }
+    curl.request(options, (err, data) => {
+        if (err) {
+            res.send(err);
+        }
 
-       var todos = JSON.parse(data).Items.map(todo => todo.content);
+        var todos = JSON.parse(data).Items;
 
-       // filter for todos of form: Completed: "title" and strip down to just title
-       var completeTodos = todos.filter(todo => todo.includes("Completed:"))
-            .map(todo => todo.substring(todo.indexOf('"') + 1, todo.length - 1));
-       var openTodos = todos.filter(todo => !todo.includes("Completed:"));
-       var actualRemainingTodos = openTodos.filter(todo => {
-           return completeTodos.indexOf(todo) === -1;
-       });
+        // filter for todos of form: Completed: "title" and strip down to just title
+        var completeTodos = todos.filter(todo => todo.content.includes("Completed:"));
+        var mappedTodos = completeTodos.map(todo => todo.content.substring(todo.content.indexOf('"') + 1, todo.content.length - 1));
+        var openTodos = todos.filter(todo => !todo.content.includes("Completed:"));
+        var actualRemainingTodos = openTodos.filter(todo => {
+            return mappedTodos.indexOf(todo.content) === -1;
+        }).map(todo => todo.content);
 
-       res.send(actualRemainingTodos);
-   })
+        var notActualRemainingTodos = openTodos.filter(todo => mappedTodos.indexOf(todo.content) != -1);
+        var toDelete = notActualRemainingTodos.concat(completeTodos).map(todo => todo.id);
+
+        // delete from todoist
+        if (toDelete.length) {
+            var deleteOptions = {
+                url: 'https://todoist.com/API/v6/sync',
+                data: {
+                    token: secrets.todoistKey,
+                    commands: '[{"type": "item_delete", "uuid": "{0}", "args": {"ids": [{1}]}}]'
+                        .replace('{0}', uuid.v4())
+                        .replace('{1}', toDelete.toString())
+                }
+            }
+            curl.request(deleteOptions, (err2, data2) => {
+                err2 ? console.dir(err2) : console.dir(data2);
+            });
+        }
+
+        res.send(actualRemainingTodos);
+    })
 });
-
-app.use('/', router);
-
-app.listen(port, () => {
-    console.log('Server listening on port ' + port);
-});
-
-
 
 
