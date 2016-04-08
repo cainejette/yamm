8fb447c401d5b0ce381146311c5e7c721f42e6ae
Caine Jette <jette@alum.mit.edu>
2016-04-08 07:58:48 -0700

    Disable annyang for now. Midori perhaps doesn't support web speech APIs...

--- src/app/controllers/voice.js
+++ src/app/controllers/voice.js
@@ -4,26 +4,25 @@ angular.module('yamm').controller('voiceCtrl',
     ['$scope', '$rootScope', '$timeout',
         function($scope, $rootScope, $timeout) {
             
-            this.command = [];
+            // this.command = [];
             
-            const commands = {
-                'go away': () => {
-                    this.command = ['go away'];
-                    $rootScope.$broadcast('hide');
-                    $timeout(() => {
-                        this.command = [];
-                    }, 2000);
-                },
-                'come back': () => {
-                    this.command = ['come back'];
-                    $rootScope.$broadcast('show');
-                    $timeout(() => {
-                        this.command = [];
-                    }, 2000);
-                }
-            };
+            // annyang.addCommands({
+            //     'go away': () => {
+            //         this.command = ['go away'];
+            //         $rootScope.$broadcast('hide');
+            //         $timeout(() => {
+            //             this.command = [];
+            //         }, 2000);
+            //     },
+            //     'come back': () => {
+            //         this.command = ['come back'];
+            //         $rootScope.$broadcast('show');
+            //         $timeout(() => {
+            //             this.command = [];
+            //         }, 2000);
+            //     }
+            // });
             
-            annyang.addCommands(commands);
-            annyang.start();
+            // annyang.start();
         }]
 )
